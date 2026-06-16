0e12921ae08918eacb247c3a09eda3bd8ae6180d
fernendete <ferdetodo98@gmail.com>
2026-06-16 14:23:00 -0400

    y6yjhhjmjm

---
+++ Store-apps/media center/script.js
@@ -0,0 +1,14 @@
+$("#userButton").on("click", function() {
+  $("#accountPanel").addClass("open");
+});
+function openAccountPanel() {
+  $("#accountPanel").addClass("open");
+}
+
+$("#closePanel").on("click", function() {
+  $("#accountPanel").removeClass("open");
+});
+$("#closePanel").on("click", function() {
+  $("#accountPanel").removeClass("open");
+});
+
